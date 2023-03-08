7e1fed0afa21ddc21e14d43c3fb42d9d2d0a692f
blackdemon1001 <blackdemon1001@gmail.com>
2023-03-07 19:25:58 -0500

    albaro-v.1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.onscroll = function() {
-	scroll = document.documentElement.scrollTop;
-	header = document.getElementById('header');
-	if (scroll > 20) {
-		header.classList.add('nav_mod')
-	}else if(scroll < 20){
-		header.classList.remove('nav_mod')
-
-	}
-}
+window.onscroll = function () {
+  scroll = document.documentElement.scrollTop;
+  header = document.getElementById("header");
+  if (scroll > 20) {
+    header.classList.add("nav_mod");
+  } else if (scroll < 20) {
+    header.classList.remove("nav_mod");
+  }
+  console.log("albaro");
+};
